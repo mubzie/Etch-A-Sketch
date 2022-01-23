9cb1e0a1612349a3dffc5de9fa308a1a1335f087
mubzie <rabiumubarak92@gmail.com>
2022-01-24 00:24:11 +0100

    complteted 90% of the project

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
 const container = document.querySelector('.grid-container');
+const blackBtn = document.querySelector('#black-mode');
+const rainbowBtn = document.querySelector('#rainbow-mode');
+const eraseBtn = document.querySelector('#erase-mode');
+const clearBtn = document.querySelector('#clear-mode');
+
+blackBtn.addEventListener('click', () => {
+        currentMode = 'black';
+});
+
+
+rainbowBtn.addEventListener('click', () => {
+        currentMode = 'randomColor'; 
+})
+
+eraseBtn.addEventListener('click', () => {
+        currentMode = 'eraser';
+})
+
+clearBtn.addEventListener('click', () => {
+    location.reload();
+})
 
 function createDiv(grid){
-    
+     
    for(let i = 0; i < grid; i++) {
        let mainGrid = document.createElement('div');
        for (j = 0; j < grid; j++) {
         let childGrid = document.createElement('div');
-        mainGrid.appendChild(childGrid).className = "child"
+        mainGrid.appendChild(childGrid).classList = "child pixel";
+        
        }
-       container.appendChild(mainGrid).className = "main-grid";
+       container.appendChild(mainGrid).classList = "main-grid";
    }
 
+   let pixels = document.querySelectorAll('.pixel');
+
+   pixels.forEach( pxl => {
+           pxl.addEventListener('mouseover', (e) => {
+                   switch(currentMode) {
+                           case 'black':
+                           e.target.style.backgroundColor = 'black'
+                           break;
+                           case 'randomColor':
+                           randomColor = '#' +Math.floor(Math.random()*16777215).toString(16).toUpperCase();
+                           e.target.style.backgroundColor = randomColor;
+                           break;
+                           case 'eraser':
+                           e.target.style.backgroundColor = 'white'
+                           break;
+                   }
+           })
+   })
 }
 createDiv(16);
-
